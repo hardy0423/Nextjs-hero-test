d6e5b237683f4502684535038db6671e981a6dd9
JHardy Raharimanantsoa <jeanhardy0423@gmail.com>
2026-01-17 21:46:38 +0300

    amelioration badge et description

--- components/hero/HeroDescription.tsx
+++ components/hero/HeroDescription.tsx
@@ -4,7 +4,7 @@ export default function HeroDescription() {
             className="max-w-lg text-base font-medium sm:max-w-2xl sm:text-lg md:text-xl"
             style={{ color: "rgba(255, 255, 255, 0.8)" }}
         >
-            Design moderne,<br className="sm:hidden" /> SEO solide, Suivi complet :<br /> on construit un site qui retient vos visiteurs et vous apporte des résultats.
+            Design moderne,<br className="sm:hidden" /> SEO solide, Suivi complet :<br /> on construit un site qui retient vos visiteurs et vous apporte des <br /> résultats.
         </p>
     );
 }
--- components/hero/HeroTitle.tsx
+++ components/hero/HeroTitle.tsx
@@ -3,7 +3,8 @@ export default function HeroTitle() {
         <h1 className="max-w-3xl text-3xl font-semibold tracking-tight sm:text-4xl md:text-5xl lg:text-6xl" style={{ color: "var(--color-light)" }}>
             Votre <span style={{ color: "var(--color-primary)" }}>site</span> doit <span style={{ color: "var(--color-primary)" }}>donner envie</span> de <span style={{ color: "var(--color-primary)" }}>rester</span>, pas de{" "}
             <span className="line-through" style={{ textDecorationColor: "var(--color-dark)" }}>
-                <span style={{ color: "var(--color-primary)" }}>revenir</span> <span style={{ color: "var(--color-light)" }}>en</span>{" "}
+                <span style={{ color: "var(--color-primary)" }}>revenir</span>{" "}
+                <span style={{ color: "var(--color-light)" }}>en</span>
                 <span style={{ color: "var(--color-primary)" }}>arrière</span>
             </span>
             <span className="inline-block w-2 h-2 bg-(--color-light) rounded-full"> </span>
