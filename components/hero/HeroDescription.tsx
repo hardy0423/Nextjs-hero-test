5640e4fc0e753a634e51f26dbf2ceb7e8675671b
JHardy Raharimanantsoa <jeanhardy0423@gmail.com>
2026-01-17 21:30:29 +0300

    amélioration du composant HeroDescription

--- components/hero/HeroDescription.tsx
+++ components/hero/HeroDescription.tsx
@@ -1,7 +1,10 @@
 export default function HeroDescription() {
     return (
-        <p className="max-w-lg text-base font-medium text-white/80 sm:max-w-2xl sm:text-lg md:text-xl">
+        <p
+            className="max-w-lg text-base font-medium sm:max-w-2xl sm:text-lg md:text-xl"
+            style={{ color: "rgba(255, 255, 255, 0.8)" }}
+        >
             Design moderne,<br className="sm:hidden" /> SEO solide, Suivi complet :<br /> on construit un site qui retient vos visiteurs et vous apporte des résultats.
         </p>
-    )
+    );
 }
