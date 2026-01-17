2aa273a1c2bf5b198a5a69dd0d6ce7c61aea3b14
JHardy Raharimanantsoa <jeanhardy0423@gmail.com>
2026-01-17 22:07:32 +0300

    amélioration affichage texte description

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -18,11 +18,11 @@ export default function Hero() {
                 </div>
             </div>
 
-            <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
+            {/* <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
 
                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[rgba(113,221,174,0.25)] blur-[120px] w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] sm:blur-[160px] md:w-[520px] md:h-[520px] md:blur-[220px]"></div>
                 <div className="absolute left-1/4 top-1/4 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[rgba(255,255,255,0.18)] blur-[100px] w-[200px] h-[200px] sm:w-[280px] sm:h-[280px] sm:blur-[140px] md:w-[360px] md:h-[360px] md:blur-[200px]"></div>
-            </div>
+            </div> */}
         </section>
     );
 }
--- components/hero/HeroTitle.tsx
+++ components/hero/HeroTitle.tsx
@@ -1,13 +1,31 @@
 export default function HeroTitle() {
     return (
-        <h1 className="max-w-3xl text-3xl font-semibold tracking-tight sm:text-4xl md:text-5xl lg:text-6xl" style={{ color: "var(--color-light)" }}>
-            Votre <span style={{ color: "var(--color-primary)" }}>site</span> doit <span style={{ color: "var(--color-primary)" }}>donner envie</span> de <span style={{ color: "var(--color-primary)" }}>rester</span>, pas de{" "}
-            <span className="line-through" style={{ textDecorationColor: "var(--color-dark)" }}>
-                <span style={{ color: "var(--color-primary)" }}>revenir</span>{" "}
-                <span style={{ color: "var(--color-light)" }}>en</span>
-                <span style={{ color: "var(--color-primary)" }}>arrière</span>
+        <h1 className="
+            max-w-3xl
+            mx-auto
+            text-center
+            text-3xl
+            sm:text-4xl
+            md:text-5xl
+            lg:text-6xl
+            font-semibold
+            tracking-tight
+            text-(--color-light)
+            leading-tight
+            mb-8
+        ">
+            Votre <span className="text-(--color-primary)">site</span> doit{" "}
+            <span className="text-(--color-primary)">donner envie</span> de{" "}
+            <span className="text-(--color-primary)">rester</span>, pas de{" "}
+            <span
+                className="line-through decoration-(--color-dark) decoration-[5px] decoration-offset-8"
+            >
+                <span className="text-(--color-primary)">revenir</span>{" "}
+                <span className="text-(--color-light)">en</span>{" "}
+                <span className="text-(--color-primary)">arrière</span>
             </span>
-            <span className="inline-block w-2 h-2 bg-(--color-light) rounded-full"> </span>
+
+            <span className="inline-block w-2 h-2 bg-(--color-light) rounded-full"></span>
         </h1>
     );
 }
